895821d615cb68db3d7ffc3db47a111aec389f07
vinil116 <vinilvvinilv8@gmail.com>
2025-09-10 18:41:22 +0530

    Initial commit

--- home.js
+++ home.js
@@ -1,28 +1,28 @@
 
 //-----------------------------------------register function starts-----------------------------
 
-// function registerOn(){
+function registerOn(){
         
-//   const register_details = {
-//     name: username.value,
-//     accountNumber: account.value,
-//     password: passwords.value
-//   };
+  const register_details = {
+    name: username.value,
+    accountNumber: account.value,
+    password: passwords.value
+  };
 
-//   if(register_details.name=="" || register_details.accountNumber=="" || register_details.password==""){
-//     alert("please fill all details");
-//   } else {
+  if(register_details.name=="" || register_details.accountNumber=="" || register_details.password==""){
+    alert("please fill all details");
+  } else {
     
-//     if(localStorage.getItem(register_details.accountNumber)){
-//       alert("this account number already exists");
-//     } else {
-      // accountNumber is key //
-      //localStorage.setItem(register_details.accountNumber, JSON.stringify(register_details));
-      //alert("account registered successfully");
-// window.location.href = "login.html";
-    // }
-  // }
-// }//
+    if(localStorage.getItem(register_details.accountNumber)){
+      alert("this account number already exists");
+    } else {
+      accountNumber is key //
+      localStorage.setItem(register_details.accountNumber, JSON.stringify(register_details));
+      alert("account registered successfully");
+window.location.href = "login.html";
+    }
+  }
+}//
 
 //-----------------------------------------register function ends-----------------------------//
 
@@ -31,36 +31,36 @@
 //-----------------------------------------login function start-----------------------------//
 
 
-// function loginOn(){
-//   const accountInput = document.getElementById("email");
-//   const passwordInput = document.getElementById("password");
+function loginOn(){
+  const accountInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
 
-//   const accountNumber = accountInput.value;
-//   const password = passwordInput.value;
+  const accountNumber = accountInput.value;
+  const password = passwordInput.value;
 
-//   if(accountNumber === "" || password === ""){
-//     alert("please fill all details");
-//     return;
-//   }
+  if(accountNumber === "" || password === ""){
+    alert("please fill all details");
+    return;
+  }
 
-//   // get localStorage
-//   const user_details = JSON.parse(localStorage.getItem(accountNumber));
+  // get localStorage
+  const user_details = JSON.parse(localStorage.getItem(accountNumber));
 
-//   if(user_details){
-//     if(user_details.password === password){
-//       alert("login success");
+  if(user_details){
+    if(user_details.password === password){
+      alert("login success");
 
-//       // 🔑 Save logged-in user globally
-//       localStorage.setItem("currentUser", JSON.stringify(user_details));
+      // 🔑 Save logged-in user globally
+      localStorage.setItem("currentUser", JSON.stringify(user_details));
 
-//       window.location.href = "detail.html"; // redirect
-//     } else {
-//       alert("incorrect password");
-//     }
-//   } else {
-//     alert("invalid account number");
-//   }
-// }
+      window.location.href = "detail.html"; // redirect
+    } else {
+      alert("incorrect password");
+    }
+  } else {
+    alert("invalid account number");
+  }
+}
 
 
 
